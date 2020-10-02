5f560a916d35e3af59be30e6dbefdd82299358f4
javi <javari02@ucm.es>
2020-10-02 17:51:58 +0200

    Gun aim, with gunDir getter, and angled sprites on 3D space

---
+++ TFG/Assets/Scripts/gunRotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunRotation : MonoBehaviour
+{
+
+    private Vector3 gunDir;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition - pos;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        gunDir = dir.normalized;
+        gunDir.z = gunDir.y;
+        gunDir.y = 0;
+
+        //Debug.DrawLine(transform.position, transform.position + gunDir, Color.green);
+        //Debug.Log(gunDir);
+    }
+
+    //this way we can get the direction of aim to shoot
+    Vector3 getGunDir() { return gunDir; }
+}
