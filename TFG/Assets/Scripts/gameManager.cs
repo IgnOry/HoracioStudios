708cba9ea8af9615e4dd8e722802c8fd382c88a9
javi <javari02@ucm.es>
2020-10-20 22:43:36 +0200

    Keyboard movement made better, implemented GameManager object and preload scene

---
+++ TFG/Assets/Scripts/gameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+
+    bool isControllerMode = false;
+
+
+    void Update()
+    {
+        
+    }
+}
