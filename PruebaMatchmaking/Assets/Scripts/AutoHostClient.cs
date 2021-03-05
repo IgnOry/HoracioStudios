5c680f9a230f8980cc5cdbdcc2200cb38189a3bf
Posna <jomart17@ucm.es>
2021-03-05 22:50:34 +0100

    Parte de pruebas de server

---
+++ PruebaMatchmaking/Assets/Scripts/AutoHostClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class AutoHostClient : MonoBehaviour
+{
+
+    [SerializeField]NetworkManager networkManager;
+
+    public void JoinLocal()
+    {
+        networkManager.networkAddress = "localhost";
+        networkManager.StartClient();
+    }
+}
---
+++ PruebaMatchmaking/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    float x = 0;
+    int vel = 0;
+
+    [SyncVar(hook = nameof(OnHolaCountChanged))]
+    int holaCount = 0;
+
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+    void Update()
+    {
+        HandleMovement();
+    }
+
+
+    public override void OnStartServer()
+    {
+        Debug.Log("Player has been spawned on the server!");
+    }
+
+    [Command]
+    void Hola()
+    {
+        Debug.Log("Received Hola from Client!");
+        holaCount += 1;
+        ReplyHola();
+    }
+
+    [TargetRpc]
+    void ReplyHola()
+    {
+        Debug.Log("Received Hola from Server!");
+    }
+
+    [ClientRpc]
+    void TooHigh()
+    {
+        Debug.Log("Too high!");
+    }
+
+    void OnHolaCountChanged(int oldCount, int newCount)
+    {
+        Debug.Log($"We had {oldCount} holas, but now we have {newCount} holas!");
+    }
+}
